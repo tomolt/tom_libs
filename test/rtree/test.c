5cc07d1fa99c6265b3ae5e57b5b1a02a999d8fa9
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2026-09-26 03:58:32 +0200

    New attempt at implementing an R*-Tree

--- test/rtree/test.c
+++ test/rtree/test.c
@@ -1,5 +1,6 @@
 #define _POSIX_C_SOURCE 200809L
 #include <stdint.h>
+#include <math.h> // for INFINITY
 
 #define DH_IMPLEMENT_HERE
 #include "dh_cuts.h"
@@ -8,53 +9,50 @@
 #include "../../tom_rtree.h"
 
 static void
-check_integrity_rec(struct rnode *node, int height, const struct raabb *bound)
+check_integrity_rec(struct rtree_node *node, int height, const struct rtree_box *bound)
 {
 	if (!node) dh_throw("Node at height %d is NULL", height);
 	if (height == 0) return;
-	for (int i = 0; i < node->degree; i++) {
-		struct raabb *aabb = &node->aabbs[i];
-		if (!(aabb->min[0] >= bound->min[0] && aabb->min[1] >= bound->min[1] && aabb->min[2] >= bound->min[2] &&
-			aabb->max[0] <= bound->max[0] && aabb->max[1] <= bound->max[1] && aabb->max[2] <= bound->max[2])) {
+	for (unsigned i = 0; i < node->count; i++) {
+		const struct rtree_box *box = &node->box[i];
+		if (!(box->min[0] >= bound->min[0] && box->min[1] >= bound->min[1] && box->min[2] >= bound->min[2] &&
+			box->max[0] <= bound->max[0] && box->max[1] <= bound->max[1] && box->max[2] <= bound->max[2])) {
 			dh_throw("AABB at height %d does not cover all children", height);
 		}
-		struct raabb aabbCopy = node->aabbs[i];
-		dh_assert(node->aabbs[i].volume == aabb_update_volume(&aabbCopy));
-		check_integrity_rec(node->child[i], height-1, aabb);
+		check_integrity_rec(node->child[i].ptr, height-1, box);
 	}
-
 }
 
 static void
 check_integrity(struct rtree *tree)
 {
-	struct raabb bound;
+	struct rtree_box bound;
 	bound.min[0] = bound.min[1] = bound.min[2] = -INFINITY;
 	bound.max[0] = bound.max[1] = bound.max[2] =  INFINITY;
 	check_integrity_rec(tree->root, tree->height, &bound);
 }
 
-static struct raabb
-synthesize_aabb(int idx)
+static struct rtree_box
+synthesize_box(int idx)
 {
-	struct raabb aabb;
-	aabb.min[0] = aabb.min[1] = aabb.min[2] = idx;
-	aabb.max[0] = aabb.min[0] + 5;
-	aabb.max[1] = aabb.min[1] + 5;
-	aabb.max[2] = aabb.min[2] + 5;
-	return aabb;
+	struct rtree_box box;
+	box.min[0] = box.min[1] = box.min[2] = idx;
+	box.max[0] = box.min[0] + 5;
+	box.max[1] = box.min[1] + 5;
+	box.max[2] = box.min[2] + 5;
+	return box;
 }
 
 static void
-check_contents_rec(struct rnode *node, int height, bool *found, int count)
+check_contents_rec(struct rtree_node *node, int height, bool *found, int count)
 {
 	if (height > 0) {
-		for (int i = 0; i < node->degree; i++) {
-			check_contents_rec(node->child[i], height-1, found, count);
+		for (unsigned i = 0; i < node->count; i++) {
+			check_contents_rec(node->child[i].ptr, height-1, found, count);
 		}
 	} else {
-		for (int i = 0; i < node->degree; i++) {
-			int idx = (intptr_t)(void *)node->child[i];
+		for (unsigned i = 0; i < node->count; i++) {
+			int idx = (intptr_t)(void *)node->child[i].ptr;
 			if (idx < 0 || idx >= count) dh_throw("Invalid element #%d emerged", idx);
 			if (found[idx]) dh_throw("Element #%d was duplicated", idx);
 			found[idx] = true;
@@ -76,59 +74,60 @@ check_contents(struct rtree *tree, int count)
 void
 test_rtree(void)
 {
-	dh_push("R-Tree");
-	struct rtree tree;
+	dh_push("R*-Tree");
 
 	dh_push("tree creation");
-	rtree_create(&tree);
-	check_integrity(&tree);
+	RTREE *tree = rtree_create();
+	check_integrity(tree);
 	dh_pop();
 	
 	dh_push("filling tree without splitting");
-	for (int i = 0; i < RTREE_MAX_DEGREE; i++) {
+	for (int i = 0; i < RTREE_MAX_CHILDREN; i++) {
 		dh_push("inserting element #%d", i);
-		struct raabb aabb = synthesize_aabb(i);
-		rtree_insert(&tree, (void *)(intptr_t)i, &aabb);
+		struct rtree_box box = synthesize_box(i);
+		rtree_insert(tree, box);
 		dh_pop();
 	}
-	check_integrity(&tree);
-	dh_assert(tree.height == 0);
-	dh_assert(tree.root->degree == RTREE_MAX_DEGREE);
-	check_contents(&tree, RTREE_MAX_DEGREE);
+	check_integrity(tree);
+	dh_assert(tree->height == 0);
+	dh_assert(tree->root->count == RTREE_MAX_CHILDREN);
+	check_contents(tree, RTREE_MAX_CHILDREN);
 	dh_pop();
 
 	dh_push("forcing tree to split");
-	struct raabb aabb = synthesize_aabb(RTREE_MAX_DEGREE);
-	rtree_insert(&tree, (void *)(intptr_t)RTREE_MAX_DEGREE, &aabb);
-	check_integrity(&tree);
-	dh_assert(tree.height == 1);
-	dh_assert(tree.root->degree == 2);
-	check_contents(&tree, RTREE_MAX_DEGREE+1);
+	struct rtree_box box = synthesize_box(RTREE_MAX_CHILDREN);
+	rtree_insert(tree, box);
+	check_integrity(tree);
+	dh_assert(tree->height == 1);
+	dh_assert(tree->root->count == 2);
+	check_contents(tree, RTREE_MAX_CHILDREN+1);
 	dh_pop();
 
+#if 0
 	dh_push("deleting a lot of elements");
-	for (int i = RTREE_MIN_DEGREE; i < RTREE_MAX_DEGREE+1; i++) {
+	for (int i = RTREE_MIN_CHILDREN; i < RTREE_MAX_CHILDREN+1; i++) {
 		dh_push("deleting element #%d", i);
-		struct raabb aabb = synthesize_aabb(i);
-		rtree_delete(&tree, (void *)(intptr_t)i, &aabb);
+		struct rtree_box box = synthesize_box(i);
+		rtree_delete(tree, (void *)(intptr_t)i, &box);
 		dh_pop();
 	}
-	check_integrity(&tree);
-	check_contents(&tree, RTREE_MIN_DEGREE);
+	check_integrity(tree);
+	check_contents(tree, RTREE_MIN_CHILDREN);
 	dh_pop();
 
 	dh_push("deleting the last remaining elements");
-	for (int i = 0; i < RTREE_MIN_DEGREE; i++) {
+	for (int i = 0; i < RTREE_MIN_CHILDREN; i++) {
 		dh_push("deleting element #%d", i);
-		struct raabb aabb = synthesize_aabb(i);
-		rtree_delete(&tree, (void *)(intptr_t)i, &aabb);
+		struct rtree_box box = synthesize_box(i);
+		rtree_delete(tree, (void *)(intptr_t)i, &box);
 		dh_pop();
 	}
 	dh_assert(tree.height == 0);
-	dh_assert(tree.root->degree == 0);
+	dh_assert(tree.root->count == 0);
 	dh_pop();
+#endif
 
-	rtree_destroy(&tree);
+	rtree_destroy(tree);
 	dh_pop();
 }
 
---
+++ tom_rtree.h
@@ -0,0 +1,388 @@
+/* R* Tree
+ */
+
+#ifndef _TOM_RTREE_H_
+#define _TOM_RTREE_H_
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#define RTREE_DIMENSION 3
+
+struct rtree_box {
+	float min[RTREE_DIMENSION];
+	float max[RTREE_DIMENSION];
+};
+
+typedef struct rtree RTREE;
+typedef uintptr_t RTREE_ID;
+
+#define RTREE_INVALID_ID SIZE_MAX
+
+RTREE *rtree_create(void);
+void   rtree_destroy(RTREE *tree);
+
+RTREE_ID rtree_insert(RTREE *tree, struct rtree_box box);
+bool     rtree_delete(RTREE *tree, RTREE_ID oid);
+size_t   rtree_search(RTREE *tree, RTREE_ID *results, size_t maxResults);
+
+// TODO bulk build, K nearest neighbor, ray cast
+
+#endif
+
+#ifdef RTREE_IMPLEMENTATION
+
+#include <stdint.h>
+
+#ifndef RTREE_malloc
+#  include <stdlib.h>
+#  define RTREE_malloc(size) malloc(size)
+#  define RTREE_free(ptr)    free(ptr)
+#endif
+
+#ifndef RTREE_rwlock_t
+
+// Pthreads
+
+#  include <pthread.h>
+
+#  define RTREE_rwlock_t              pthread_rwlock_t
+#  define RTREE_rwlock_init(lockp)    pthread_rwlock_init(lockp, NULL)
+#  define RTREE_rwlock_destroy(lockp) pthread_rwlock_destroy(lockp)
+#  define RTREE_lock_read(lockp)      pthread_rwlock_rdlock(lockp)
+#  define RTREE_lock_write(lockp)     pthread_rwlock_wrlock(lockp)
+#  define RTREE_unlock_read(lockp)    pthread_rwlock_unlock(lockp)
+#  define RTREE_unlock_write(lockp)   pthread_rwlock_unlock(lockp)
+
+#endif
+
+#ifndef RTREE_assert
+#  include <assert.h>
+#  define RTREE_assert(cond) assert(cond) 
+#endif
+
+#define RTREE_MIN(a,b) ((a) < (b) ? (a) : (b))
+#define RTREE_MAX(a,b) ((a) > (b) ? (a) : (b))
+
+#define RTREE_SORT_INLINE(elemtype, compare, array, count)\
+	do {\
+		for (size_t _i = 1; _i < (count); _i++) {\
+			for (size_t _j = _i; _j > 0 && compare((array)[_j - 1], (array)[_j]) > 0; _j--) {\
+				elemtype _tmp = (array)[_j];\
+				(array)[_j] = (array)[_j - 1];\
+				(array)[_j - 1] = _tmp;\
+			}\
+		}\
+	} while (0)
+
+#define RTREE_GET_BIT(val, idx) (((val) >> (idx)) & 1u)
+#define RTREE_SET_BIT(val, idx) ((val) |= (uint64_t)1u << (idx))
+
+// TODO determine actual values
+#define RTREE_MIN_CHILDREN 26
+#define RTREE_MAX_CHILDREN 64
+#define RTREE_MAX_HEIGHT   64
+
+struct rtree_node {
+	struct rtree_box box[RTREE_MAX_CHILDREN];
+	union rtree_child {
+    		RTREE_ID           oid; // If node is a leaf
+    		struct rtree_node *ptr; // if node is an internal node
+	} child[RTREE_MAX_CHILDREN];
+	unsigned count;
+};
+
+struct rtree {
+	RTREE_rwlock_t     lock;
+	struct rtree_node *root;
+	unsigned           height;
+};
+
+static bool
+rtree_boxes_overlap(struct rtree_box boxA, struct rtree_box boxB)
+{
+	for (unsigned axis = 0; axis < RTREE_DIMENSION; axis++) {
+		if (boxA.max[axis] <= boxB.min[axis] || boxB.max[axis] <= boxA.min[axis]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+static struct rtree_box
+rtree_box_union(struct rtree_box boxA, struct rtree_box boxB)
+{
+	struct rtree_box boxU;
+	for (unsigned axis = 0; axis < RTREE_DIMENSION; axis++) {
+		boxU.min[axis] = RTREE_MIN(boxA.min[axis], boxB.min[axis]);
+		boxU.max[axis] = RTREE_MAX(boxA.max[axis], boxB.max[axis]);
+	}
+	return boxU;
+}
+
+static struct rtree_box
+rtree_box_intersection(struct rtree_box boxA, struct rtree_box boxB)
+{
+	struct rtree_box boxI;
+	for (unsigned axis = 0; axis < RTREE_DIMENSION; axis++) {
+		boxI.min[axis] = RTREE_MAX(boxA.min[axis], boxB.min[axis]);
+		boxI.max[axis] = RTREE_MIN(boxA.max[axis], boxB.max[axis]);
+		if (boxI.min[axis] > boxI.max[axis]) {
+			boxI.max[axis] = boxI.min[axis];
+		}
+	}
+	return boxI;
+}
+
+static float
+rtree_box_margin(struct rtree_box box)
+{
+	float margin = 0.0f;
+	for (unsigned axis = 0; axis < RTREE_DIMENSION; axis++) {
+		margin += box.max[axis] - box.min[axis];
+	}
+	return margin;
+}
+
+static float
+rtree_box_area(struct rtree_box box)
+{
+	float area = 1.0f;
+	for (unsigned axis = 0; axis < RTREE_DIMENSION; axis++) {
+    		area *= box.max[axis] - box.min[axis];
+	}
+	return area;
+}
+
+/* Path does not include the (returned) leaf node.
+ */
+static struct rtree_node *
+rtree_choose_subtree(RTREE *tree, struct rtree_box newBox, struct rtree_node *path[], unsigned *lengthOut)
+{
+	if (!tree || !tree->root) return NULL;
+
+	struct rtree_node *node = tree->root;
+	unsigned level = tree->height - 1;
+	unsigned length = 0;
+	while (level > 0) {
+		path[length++] = node;
+
+		RTREE_assert(node->count > 0);
+
+		float costs[RTREE_MAX_CHILDREN];
+#if 0
+		if (level == 1) {
+			// Minimum overlap costs
+			for (unsigned i = 0; i < node->count; i++) {
+				struct rtree_box boxU = rtree_box_union(node->box[i], newBox);
+			}
+		} else {
+#endif
+			// Minimum area costs
+			for (unsigned i = 0; i < node->count; i++) {
+				struct rtree_box boxU = rtree_box_union(node->box[i], newBox);
+				costs[i] = rtree_box_area(boxU);
+			}
+#if 0
+		}
+#endif
+
+		unsigned bestIdx = 0;
+		for (unsigned i = 1; i < node->count; i++) {
+			if (costs[i] < costs[bestIdx]) {
+				bestIdx = i;
+			}
+		}
+
+		node = node->child[bestIdx].ptr;
+		level--;
+	}
+
+	*lengthOut = length;
+	return node;
+}
+
+static unsigned
+rtree_choose_split_axis(const struct rtree_box box[], unsigned count)
+{
+	float scores[RTREE_DIMENSION];
+	for (unsigned axis = 0; axis < RTREE_DIMENSION; axis++) {
+
+		unsigned order[RTREE_MAX_CHILDREN];
+		for (unsigned i = 0; i < count; i++) {
+			order[i] = i;
+		}
+
+#define RTREE_COMPARE_ALONG_AXIS(a, b)\
+	box[a].min[axis] < box[b].min[axis] ? -1 :\
+	box[a].min[axis] > box[b].min[axis] ?  1 :\
+	box[a].max[axis] < box[b].max[axis] ? -1 :\
+	box[a].max[axis] > box[b].max[axis] ?  1 :\
+	0
+
+		RTREE_SORT_INLINE(unsigned, RTREE_COMPARE_ALONG_AXIS, order, count);
+
+#undef RTREE_COMPARE_ALONG_AXIS
+
+		RTREE_assert(count >= 2 * RTREE_MIN_CHILDREN);
+
+		for (unsigned d = RTREE_MIN_CHILDREN; d < count - RTREE_MIN_CHILDREN; d++) {
+		}
+		
+		scores[axis] = 0.0f;
+	}
+
+	unsigned bestAxis = 0;
+	for (unsigned axis = 1; axis < RTREE_DIMENSION; axis++) {
+    		if (scores[axis] < scores[bestAxis]) {
+        		bestAxis = axis;
+    		}
+	}
+	return bestAxis;
+}
+
+#if 1
+// Performs both the 'ChooseSplitIndex' and 'Distribute' Algorithms
+static struct rtree_node *
+rtree_distribute(struct rtree_node *node, unsigned axis)
+{
+	unsigned order[RTREE_MAX_CHILDREN];
+	for (unsigned i = 0; i < node->count; i++) {
+		order[i] = i;
+	}
+
+#define RTREE_COMPARE_ALONG_AXIS(a, b)\
+	node->box[a].min[axis] < node->box[b].min[axis] ? -1 :\
+	node->box[a].min[axis] > node->box[b].min[axis] ?  1 :\
+	node->box[a].max[axis] < node->box[b].max[axis] ? -1 :\
+	node->box[a].max[axis] > node->box[b].max[axis] ?  1 :\
+	0
+
+	RTREE_SORT_INLINE(unsigned, RTREE_COMPARE_ALONG_AXIS, order, node->count);
+
+#undef RTREE_COMPARE_ALONG_AXIS
+
+	unsigned splitIndex;
+	for (unsigned d = RTREE_MIN_CHILDREN; d < node->count - RTREE_MIN_CHILDREN; d++) {
+		
+	}
+
+	struct rtree_box sortedBoxes[RTREE_MAX_CHILDREN];
+	for (unsigned i = 0; i < node->count; i++) {
+		sortedBoxes[i] = node->box[order[i]];
+	}
+
+	union rtree_child sortedChildren[RTREE_MAX_CHILDREN];
+	for (unsigned i = 0; i < node->count; i++) {
+		sortedChildren[i] = node->child[order[i]];
+	}
+
+	struct rtree_node *other = RTREE_malloc(sizeof *other);
+	if (!other) {
+		// TODO
+		return NULL;
+	}
+
+	unsigned count = node->count;
+	memcpy(node->box,    sortedBoxes,    splitIndex * sizeof(struct rtree_box));
+	memcpy(node->child,  sortedChildren, splitIndex * sizeof(union rtree_child));
+	memcpy(other->box,   sortedBoxes    + splitIndex, (node->count - splitIndex) * sizeof(struct rtree_box));
+	memcpy(other->child, sortedChildren + splitIndex, (node->count - splitIndex) * sizeof(union rtree_child));
+
+	node->count  = splitIndex;
+	other->count = count - splitIndex;
+
+	return other;
+}
+
+static struct rtree_node *
+rtree_split(struct rtree_node *node)
+{
+	unsigned axis = rtree_choose_split_axis(node->box, node->count);
+	return rtree_distribute(node, axis);
+}
+#endif
+
+#if 0
+static void
+rtree_reinsert()
+{
+	for (children and candidate) {
+    		
+	}
+}
+#endif
+
+#if 0
+static void
+rtree_treat_overflow(RTREE *tree, struct rtree_node *node, unsigned level, uint64_t *overflowed)
+{
+	if (node != tree->root && !RTREE_GET_BIT(*overflowed, level)) {
+    		rtree_reinsert();
+	} else {
+    		rtree_split();
+	}
+}
+#endif
+
+#if 0
+static void
+rtree_insert_at_level(RTREE *tree, struct rtree_box newBox)
+{
+	struct rtree_node *path[RTREE_MAX_CHILDREN];
+	unsigned length;
+	struct rtree_node *leaf = rtree_choose_subtree(tree, path, &length);
+
+	if (leaf->count < RTREE_MAX_CHILDREN) {
+		// store
+		return;
+	}
+
+	if (subtree->count == RTREE_MAX_CHILDREN) {
+		rtree_treat_overflow();
+	}
+
+	// Propagate split upward, updating the boxes
+	for () {
+	}
+}
+#endif
+
+RTREE *
+rtree_create(void)
+{
+	RTREE *tree = RTREE_malloc(sizeof *tree);
+	if (!tree) return NULL;
+
+	RTREE_rwlock_init(&tree->lock);
+	tree->root = NULL;
+	tree->height = 0;
+	
+	return tree;
+}
+
+void
+rtree_destroy(RTREE *tree)
+{
+	if (!tree) return;
+
+	RTREE_rwlock_destroy(&tree->lock);
+
+	free(tree);
+}
+
+RTREE_ID
+rtree_insert(RTREE *tree, struct rtree_box box)
+{
+	if (!tree) return false;
+
+	RTREE_lock_write(&tree->lock);
+
+	//rtree_insert_at_level();
+
+	RTREE_unlock_write(&tree->lock);
+
+	return true;
+}
+
+#endif
